92e5fe6745e6941510bbe5051348022274bc8a4a
TanV+TinhN <tan.vu+tinh.nguyen@eastagile.com>
2016-11-11 13:49:09 +0700

    [f] 009 - Refactor user table
    [#74MlWbQm] https://trello.com/c/74MlWbQm

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,16 @@
+import { logger } from 'logs/winston-logger';
+import { GET_STARTED_PAYLOAD } from 'utils/constants';
+
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    userId: DataTypes.STRING,
+    userId: {
+      type: DataTypes.STRING,
+      primaryKey: true
+    },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    gender: DataTypes.STRING
+    gender: DataTypes.STRING,
+    currentPayload: DataTypes.STRING
   }, {
     freezeTableName: true,
     classMethods: {
@@ -12,8 +19,11 @@ export default (sequelize, DataTypes) => {
           userId: userId,
           firstName: userProfile.first_name,
           lastName: userProfile.last_name,
-          gender: userProfile.gender
+          gender: userProfile.gender,
+          currentPayload: GET_STARTED_PAYLOAD
         };
+
+        logger.log('info', 'Save to user profile to database %j', fullUserProfile);
 
         return User.findOrCreate({
           attributes: ['userId', 'firstName', 'lastName', 'gender'],
