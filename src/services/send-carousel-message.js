8baf19054c748f843e7701952767eb9c4b0f42f4
TinhN+TanV <tinh.nguyen+tan.vu@eastagile.com>
2016-11-15 14:42:54 +0700

    [f] 024 - Merge and resolve conflict on merging develop
    [#IVPkBvOD]https://trello.com/c/IVPkBvOD

--- src/services/send-carousel-message.js
+++ src/services/send-carousel-message.js
@@ -4,12 +4,10 @@ export const sendCarouselMessage = (recipientId, elements) => {
   let elementsData = [];
   for (let element of elements) {
     const elementData = {
-      // TODO: we're waiting for the data from client. Fix it later.
+      // FIXME: fix hard code title, image_url and subtitle, we're waiting for the data from client. Fix it later.
       title: 'The standard Lorem Ipsum passage, used since the 1500s',
       item_url: element,
-      // TODO: we're waiting for the data from client. Fix it later.
       image_url: 'https://cdn.vectorstock.com/i/composite/39,73/beautiful-doctor-vector-323973.jpg',
-      // TODO: we're waiting for the data from client. Fix it later.
       subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       buttons: [{
         type: 'web_url',
