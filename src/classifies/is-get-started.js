3eaac97bae9a81e24c19b788607e5e36f333a4b9
TinhN+TanV <tinh.nguyen+tan.vu@eastagile.com>
2016-11-15 11:35:37 +0700

    [c] 027 - Remove classifiers and transporters
    [#Fq9kyCqz] https://trello.com/c/Fq9kyCqz

--- src/classifies/is-get-started.js
+++
@@ -1,5 +0,0 @@
-import { FACEBOOK_GET_STARTED_PAYLOAD } from 'utils/constants';
-
-export const isGetStarted = function(messageEvent) {
-  return messageEvent && messageEvent.postback && messageEvent.postback.payload === FACEBOOK_GET_STARTED_PAYLOAD;
-};
