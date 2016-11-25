687d45250f1b044050229adb4c0b9a3eac9ebbc3
TanV+TinhN <tan.vu+tinh.nguyen@eastagile.com>
2016-11-25 17:08:57 +0700

    [f] 049 - Add `Xem bai viet` button for each article
    [#WSNdrxOv] https://trello.com/c/WSNdrxOv

--- db/migrations/20161125055654-update-disease-add-akaname-subtitle-and-image.js
+++ db/migrations/20161125055654-update-disease-add-akaname-subtitle-and-image.js
@@ -2,17 +2,17 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    queryInterface.addColumn('Disease', 'akaName', Sequelize.STRING).then(function() {
-      queryInterface.addColumn('Disease', 'subtitle', Sequelize.STRING).then(function () {
-        queryInterface.addColumn('Disease', 'image', Sequelize.STRING)
+    return queryInterface.addColumn('Disease', 'akaName', Sequelize.STRING).then(function() {
+      return queryInterface.addColumn('Disease', 'subtitle', Sequelize.STRING).then(function () {
+        return queryInterface.addColumn('Disease', 'image', Sequelize.STRING)
       });
     });
   },
 
   down: function (queryInterface, Sequelize) {
-    queryInterface.removeColumn('Disease', 'image').then(function() {
-      queryInterface.removeColumn('Disease', 'subtitle').then(function() {
-        queryInterface.removeColumn('Disease', 'akaName');
+    return queryInterface.removeColumn('Disease', 'image').then(function() {
+      return queryInterface.removeColumn('Disease', 'subtitle').then(function() {
+        return queryInterface.removeColumn('Disease', 'akaName');
       });
     });
   }
