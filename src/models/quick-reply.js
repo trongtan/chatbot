76cfac12991f5dc08d744e18a94a4d06e5714ef6
TanV+TinhN <TanV+TinhN@eastagile.com>
2016-12-09 11:04:44 +0700

    [f] 064 - dynamic quick reply
    [#HCrKoLLO] https://trello.com/c/HCrKoLLO

--- src/models/quick-reply.js
+++ src/models/quick-reply.js
@@ -4,7 +4,25 @@ export default (sequelize, DataTypes) => {
     title: DataTypes.STRING,
     imageUrl: DataTypes.STRING
   }, {
-    freezeTableName: true
+    freezeTableName: true,
+    classMethods: {
+      findByGroup: (groupName) => {
+        return sequelize.query('SELECT  "RawQuickReply"."id", "RawQuickReply"."title",  "RawQuickReply"."imageUrl", "RawQuickReply"."contentType", "RawQuickReply"."payloadId", "Group"."name" ' +
+          'FROM ' +
+          '(SELECT "QuickReply"."id", "QuickReply"."title", "QuickReply"."payloadId", "QuickReply"."imageUrl", "QuickReply"."contentType" FROM "QuickReply" ' +
+          'INNER JOIN "Group" ' +
+          'ON "QuickReply"."groupId" = "Group"."id" AND "Group"."name" like $groupName) AS "RawQuickReply" ' +
+          'INNER JOIN "Group" ' +
+          'ON "RawQuickReply"."payloadId" = "Group"."id"',
+          {
+            type: sequelize.QueryTypes.SELECT,
+            bind: {
+              groupName: groupName
+            }
+          }
+        ).then(result => result.length > 0 ? result : null);
+      }
+    }
   });
   return QuickReply;
 };
